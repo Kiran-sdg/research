0548274b1ba4e32da84eea23f01c28d04fb73ea7
Kiran-sdg <m2970494@gmail.com>
2026-05-07 15:21:45 -0400

    Create impl.java

---
+++ impl.java
@@ -0,0 +1,83 @@
+package org.ncsecu.aem.site.core.models.impl;
+
+import com.adobe.acs.commons.genericlists.GenericList;
+import com.adobe.acs.commons.genericlists.GenericList.Item;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.ncsecu.aem.site.core.models.NcsecuTrueCarMake;
+import org.ncsecu.aem.site.core.models.NcsecuTrueCarSearch;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Model(
+    adaptables = Resource.class,
+    adapters = NcsecuTrueCarSearch.class,
+    resourceType = "ncsecu/components/truecar-search",
+    defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
+public class NcsecuTrueCarSearchImpl implements NcsecuTrueCarSearch {
+
+    @ValueMapValue
+    private String makeListPath;
+
+    @SlingObject
+    private ResourceResolver resourceResolver;
+
+    private List<NcsecuTrueCarMake> makes;
+
+    @PostConstruct
+    protected void init() {
+
+        makes = new ArrayList<>();
+
+        if (StringUtils.isBlank(makeListPath) || resourceResolver == null) {
+            return;
+        }
+
+        Resource listResource = resourceResolver.getResource(makeListPath);
+
+        if (listResource == null) {
+            return;
+        }
+
+        GenericList genericList = listResource.adaptTo(GenericList.class);
+
+        if (genericList == null) {
+            return;
+        }
+
+        for (Item item : genericList.getItems()) {
+
+            if (item == null) {
+                continue;
+            }
+
+            String title = item.getTitle();
+            String value = item.getValue();
+
+            if (StringUtils.isNotBlank(title)
+                    && StringUtils.isNotBlank(value)) {
+
+                makes.add(new NcsecuTrueCarMake(title, value));
+            }
+        }
+    }
+
+    @Override
+    public List<NcsecuTrueCarMake> getMakes() {
+        return makes == null ? Collections.emptyList() : makes;
+    }
+
+    @Override
+    public String getMakeListPath() {
+        return makeListPath;
+    }
+}
